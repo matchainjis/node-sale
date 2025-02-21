b9837b11302b86416a354d01d470ab833df274c5
tenshou <pamiheevcorp@gmail.com>
2025-02-21 16:33:57 +0700

    feat: MATCH-12 add pqueue

--- src/modules/pool/actions/getAccountPools.ts
+++ src/modules/pool/actions/getAccountPools.ts
@@ -1,5 +1,7 @@
+import PQueue from 'p-queue';
+
 import { api, cacheTags } from 'modules/api';
-import { ZERO } from 'modules/common/const';
+import { BASE_PQUEUE_CONFIG, ZERO } from 'modules/common/const';
 import { getAccountPoolEndpoint } from 'modules/pool/actions/getAccountPool';
 
 import { IAccountPool } from '../types';
@@ -14,17 +16,22 @@ export const { useGetAccountPoolsQuery } = api.injectEndpoints({
   endpoints: build => ({
     getAccountPools: build.query<IAccountPool[], IGetAccountPoolsArgs>({
       queryFn: async ({ addresses }, { dispatch }) => {
-        const result = await Promise.all(
-          addresses.map(async address =>
+        const queue = new PQueue(BASE_PQUEUE_CONFIG);
+
+        const requests = addresses.map(address =>
+          queue.add(() =>
             dispatch(getAccountPoolEndpoint.initiate({ address })),
           ),
         );
 
-        const pools = result
-          .filter(
-            pool => !!pool.data && pool.data.stakedAmount.isGreaterThan(ZERO),
+        const results = await Promise.allSettled(requests);
+
+        const pools = results
+          .map(result =>
+            result.status === 'fulfilled' ? result.value?.data : null,
           )
-          .map(pool => pool.data as IAccountPool);
+          .filter(pool => !!pool && pool?.stakedAmount.isGreaterThan(ZERO))
+          .map(pool => pool as IAccountPool);
 
         return { data: pools };
       },
