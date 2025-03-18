5c86d3a401159af4f7bbcaa3fabe468b1a8cf4ae
Peter Mikheev <27726003+tenshouuu@users.noreply.github.com>
2025-03-18 12:01:06 +0700

    feat: MATCH-21 add new contracts (#18)

--- src/modules/api/const.ts
+++ src/modules/api/const.ts
@@ -1,6 +1,6 @@
 export const MAIN_TOKEN_ADDRESS = import.meta.env.VITE_MAIN_TOKEN_ADDRESS;
 
 export const UNSTAKE_PERIOD_DAYS =
-  import.meta.env.VITE_ENV === 'prod' ? 7 : 0.00347;
+  import.meta.env.VITE_ENV === 'dev' ? 0.00347 : 7;
 
 export const IPFS_PREFIX = 'ipfs://';
